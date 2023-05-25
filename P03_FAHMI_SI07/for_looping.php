107a986990a8be7855f47e1f126437526960bd9f
fahmsi <fahm22225si@student.nurulfikri.ac.id>
2023-05-25 13:18:43 +0700

    fix rental_mobil

--- P03_FAHMI_SI07/for_looping.php
+++ P03_FAHMI_SI07/for_looping.php
@@ -8,6 +8,6 @@ echo "<br>";
 $array = ['kucing','anjing','badak','kapibara'];
 //  count berfungsi untuk menghitung panjang array
 for ($i = 0; $i < count($array);$i++){
-    echo $array[$i] "<br>";
+    echo $array[$i], "<br>";
 }
 
--- P03_FAHMI_SI07/foreach_looping.php
+++ P03_FAHMI_SI07/foreach_looping.php
@@ -1,5 +1,5 @@
 <?php
-$arrayHewan = ['kambing','badak','sapi','kuda']
+$arrayHewan = ['kambing','badak','sapi','kuda'];
 
 foreach ($arrayHewan as $key => $value){
     echo "$key adalah  $value  <br>" ;
